0d97e529129cfd4d39f7af40bffcb1c78d2b60c4
Yann Collet <yann.collet.73@gmail.com>
2020-03-01 15:56:09 -0800

    test adding stdio

--- tests/collisions/threading.h
+++ tests/collisions/threading.h
@@ -41,7 +41,8 @@ extern "C" {
 #  define WIN32_LEAN_AND_MEAN
 #endif
 
-#include "windows.h"
+#include <windows.h>
+#include <stdio.h>
 
 /* mutex */
 #define ZSTD_pthread_mutex_t           CRITICAL_SECTION

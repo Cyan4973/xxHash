4dd7933794ce0decd62ad40d47856279699100fb
P.M <60963077+goodengineer@users.noreply.github.com>
2021-03-12 09:23:22 +0200

    Update xxh_x86dispatch.h

--- xxh_x86dispatch.h
+++ xxh_x86dispatch.h
@@ -55,7 +55,6 @@ XXH_PUBLIC_API XXH_errorcode XXH3_128bits_update_dispatch(XXH3_state_t* state, c
 }
 #endif
 
-
 /* automatic replacement of XXH3 functions.
  * can be disabled by setting XXH_DISPATCH_DISABLE_REPLACE */
 #ifndef XXH_DISPATCH_DISABLE_REPLACE

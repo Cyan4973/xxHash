665424e72078511238fe9053159dc073244672e8
Yann Collet <cyan@fb.com>
2021-08-25 15:30:42 -0700

    added variant XXH3_64bits_withSecretandSeed()
    and its companion XXH3_generateSecret_fromSeed().
    
    The new variant uses @seed for "small" keys,
    and @secret for "large" keys.
    When combined with the new generator,
    it provides exactly the same results as _withSeed(),
    with the benefit of pre-caculated @secret for "large keys",
    resulting in a speed boost for "not too large" keys (<1 KB).

--- cli/xsum_sanity_check.c
+++ cli/xsum_sanity_check.c
@@ -378,6 +378,16 @@ static void XSUM_testXXH3(const void* data, const XSUM_testdata64_t* testData)
         XSUM_checkResult64(Dresult, Nresult);
     }
 
+    /* check that the combination of
+     * XXH3_generateSecret_fromSeed() and XXH3_64bits_withSecretandSeed()
+     * results in exactly the same hash generation as XXH3_64bits_withSeed() */
+    {   char secretBuffer[XXH3_SECRET_DEFAULT_SIZE+1];
+        char* const secret = secretBuffer + 1;  /* intentional unalignment */
+        XXH3_generateSecret_fromSeed(secret, seed);
+        {   XSUM_U64 const Dresult = XXH3_64bits_withSecretandSeed(data, len, secret, XXH3_SECRET_DEFAULT_SIZE, seed);
+            XSUM_checkResult64(Dresult, Nresult);
+    }   }
+
     /* streaming API test */
     {   XXH3_state_t* const state = XXH3_createState();
         assert(state != NULL);

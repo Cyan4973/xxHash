6490f8bbedee1422e109709629c053526c582679
t-mat <t-mat@users.noreply.github.com>
2025-08-23 08:35:16 +0900

    Fix line number

--- tests/windows/00-test-all.bat
+++ tests/windows/00-test-all.bat
@@ -4,6 +4,7 @@ set /a errorno=1
 : _E = set the line number of the first !__! - 1
 set /a _E=8-1
 set "__=set /a _E+=1"
+
 !__! && for /f "delims=. tokens=1,2" %%E in ("%~n0%~x0") do set "TEST_NAME=%%E"
 !__! && for /F %%E in ('forfiles /m "%~nx0" /c "cmd /c echo 0x1b"') do set "_ESC=%%E"
 !__! && set "ORG_DIR=!CD!"

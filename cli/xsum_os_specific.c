50905599aec4504a04b196bfeb7167bbb9fd88b7
t-mat <t-mat@users.noreply.github.com>
2025-08-23 10:26:46 +0900

    Fix XXHSUM_WIN32_LONGPATH detection logic

--- cli/xsum_os_specific.c
+++ cli/xsum_os_specific.c
@@ -158,7 +158,7 @@ int main(int argc, const char* argv[])
 #else
 #  include <windows.h>
 #  include <wchar.h>
-#  if XXHSUM_WIN32_LONGPATH
+#  if defined(XXHSUM_WIN32_LONGPATH) && XXHSUM_WIN32_LONGPATH
 #    include <pathcch.h> /* PathCchCanonicalizeEx, PathCchCombineEx */
 #  endif
 
@@ -214,7 +214,7 @@ static char* XSUM_narrowString(const wchar_t *str, int *lenOut)
  */
 static wchar_t* XSUM_widenStringAsExtendedLengthPath(const char* path)
 {
-#if XXHSUM_WIN32_LONGPATH
+#if defined(XXHSUM_WIN32_LONGPATH) && XXHSUM_WIN32_LONGPATH
     wchar_t* const wide_path = XSUM_widenString(path, NULL);  /* path in wchar_t */
     size_t const path_len = strlen(path);
     int const starts_with_extended_prefix = path_len >= 4 && path[0] == '\\' && path[1] == '\\' && path[2] == '?' && path[3] == '\\';
